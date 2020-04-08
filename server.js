5124374a80e07a2df79c22bc0b334b11f6573885
Jorge Zreik <jorgezreik@Jorges-MacBook-Pro.local>
2020-04-08 01:31:22 -0400

    Adding Reis functionality

--- server.js
+++ server.js
@@ -18,22 +18,27 @@ app.get('/', function (req, res) {
 app.post('/', function (req, res) {
   let city = req.body.city;
   let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
-  request(url, function (err, response, body) {
-    if (err) {
-      res.render('index', {weather: null, error: 'Error, please try again'});
-    }
-    else {
-      let weather = JSON.parse(body);
-      if (weather.main == undefined) {
+  if (city == 'Dover' || city == 'Dover, Massachusetts' || city == 'Dover, MA') {
+    res.render('index', {weather: 'Hi Reis', error: null});
+  }
+  else {
+    request(url, function (err, response, body) {
+      if (err) {
         res.render('index', {weather: null, error: 'Error, please try again'});
-        console.log(url);
       }
       else {
-        let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
-        res.render('index', {weather: weatherText, error: null});
+        let weather = JSON.parse(body);
+        if (weather.main == undefined) {
+          res.render('index', {weather: null, error: 'Error, please try again'});
+          console.log(url);
+        }
+        else {
+          let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+          res.render('index', {weather: weatherText, error: null});
+        }
       }
-    }
-  });
+    });
+  }
 });
 
 app.listen(port, () => {
